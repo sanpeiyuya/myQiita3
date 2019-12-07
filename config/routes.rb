1083d7e98981a94d9ddaad3fc06d8b6deb1384df
sanpeiyuya <kingdommakao@gmail.com>
2019-12-07 14:41:31 +0900

    scaffoldを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root "articles#index"
-  resources :articles
-=======
->>>>>>> parent of 814ac07... rails g scaffold
+  root 'post#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
