ddbaced14d6d4d2410ea735c2f36b7d5352cb40d
sanpeiyuya <kingdommakao@gmail.com>
2019-12-19 04:08:40 +0900

    posts controllerを入れ直す

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'post#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "posts/:id" => "posts#show"
+  resources :posts
 end
