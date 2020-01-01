9d9dd0d6fe354ab737541cb7c7d91396fc87cdcd
sanpeiyuya <kingdommakao@gmail.com>
2020-01-02 00:45:13 +0900

    userのnewとshow画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name],email:params[:email],nickname:params[:nickname])
+    if @user.save
+      flash[:notice]= "新規登録できました"
+      redirect_to users_path
+    else
+      render("users/new")
+    end
+  end
 end
