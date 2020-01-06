a9d0875e072edb0d967f4e5be1f15ba2a5890a53
sanpeiyuya <kingdommakao@gmail.com>
2020-01-07 08:10:25 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,13 @@ class LikesController < ApplicationController
   def create
     @like =Like.new(user_id: @current_user.id ,post_id: params[:post_id])
     @like.save
-    redirect_to root_path ,notice:"いいね！"
+    redirect_to post_path(params[:post_id]) ,notice:"いいね！"
   end
 
   def destroy
     @like = Like.find_by(user_id: @current_user.id ,post_id: params[:id].to_i)
     @like.destroy
-    redirect_to root_path  ,notice:"いいねを削除しました"
+    redirect_to post_path(params[:post_id])  ,notice:"いいねを削除しました"
   end
   # private
   # def likes_params
