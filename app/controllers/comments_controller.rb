577772be602671aa3ba7e21172420a3c2ba7abec
sanpeiyuya <kingdommakao@gmail.com>
2020-01-21 15:18:31 +0900

    comment controller respond_to json

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,30 @@
 class CommentsController < ApplicationController
   def create
-    comment=Comment.new(text:comment_params[:text],post_id: comment_params[:post_id],user_id:@current_user.id)
-    if comment.save
-      redirect_to post_path(params[:post_id]),notice:"コメント作成しました。"
-    else
-      redirect_to post_path(params[:post_id]),notice:"コメントを入力してください"
+    @comment=Comment.create(comment_params)
+    respond_to do |format|
+      format.html {redirect_to post_path(params[:post_id]),notice:"コメント作成しました。"}
+      format.json
+      # if @comment.save
+      #   redirect_to post_path(params[:post_id]),notice:"コメント作成しました。"
+      # else
+      #   redirect_to post_path(params[:post_id]),notice:"コメントを入力してください"
+      # end  }
     end
+    
   end
 
   def destroy
     comment = Comment.find(params[:id])
     comment.delete
-    redirect_to post_path(params[:post_id])
+      respond_to do |format|
+        format.js
+        format.html
+      end
+    # redirect_to post_path(params[:post_id])
   end
 
   private 
   def comment_params
-    params.permit(:text,:post_id)
+    params.permit(:text,:post_id).merge(user_id: @current_user.id)
   end
 end
